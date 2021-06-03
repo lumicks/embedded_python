58d2d69d0658c1a2bdbe13fe370f7b74e769ebcb
Dean Moldovan <dean0x7d@gmail.com>
2021-06-03 20:27:13 +0200

    Fix slow `find_package(Python)` on Windows
    It seems that, even though we give it an exact `Python_ROOT_DIR`,
    CMake does still search the system for all other Python versions.
    In the end, it does correctly select the one in `Python_ROOT_DIR`
    as we specified but the this entire process is very slow: between
    20 seconds and a full minute.
    
    The fix is to give CMake more info: tell it which version of Python
    is at `Python_ROOT_DIR` and request it to be EXACT. This seems to
    allow it to return immediately without considering others. It brings
    the time down to ~3 seconds. And if we also tell it the exact
    location of `Python_EXECUTABLE`, the time is down to ~1 second.

--- embedded_python.cmake
+++ embedded_python.cmake
@@ -1,2 +1,9 @@
+# A hint for `find_package(Python)`
 set(Python_ROOT_DIR "${CONAN_EMBEDDED_PYTHON_ROOT}/embedded_python")
-find_package(Python COMPONENTS Interpreter Development REQUIRED)
+
+if(WIN32) # Extra hint to speed up the find (not needed for correctness)
+    set(Python_EXECUTABLE "${Python_ROOT_DIR}/python.exe")
+endif()
+
+find_package(Python ${CONAN_USER_EMBEDDED_PYTHON_pyversion} EXACT REQUIRED
+             COMPONENTS Interpreter Development)
