e6ee59634882ce138be07076a4e6c55483417d89
Dean Moldovan <dean0x7d@gmail.com>
2023-07-11 18:15:05 +0200

    test_package: Fix deprecation warnings
    Python 3.8 introduced a new configuration API that provides a lot more
    control, especially for embedding. The old API has been marked as
    deprecated in Python 3.11 (e.g. functions like `Py_SetPythonHome()`).

--- core/test_package/src/main.cpp
+++ core/test_package/src/main.cpp
@@ -3,10 +3,23 @@
 #include <filesystem>
 
 int main(int argc, const char* argv[]) {
+    auto config = PyConfig{};
+    PyConfig_InitIsolatedConfig(&config);
+
     const auto bin = std::filesystem::path(argv[0]).parent_path();
-    const auto python_home = (bin / "python").wstring();
-    Py_SetPythonHome(python_home.data());
-    Py_Initialize();
+    const auto python_home = (bin / "python").string();
+    if (auto status = PyConfig_SetBytesString(&config, &config.home, python_home.c_str());
+        PyStatus_Exception(status)) {
+        PyConfig_Clear(&config);
+        return 1;
+    }
+
+    if (auto status = Py_InitializeFromConfig(&config); PyStatus_Exception(status)) {
+        PyConfig_Clear(&config);
+        return 1;
+    }
+    PyConfig_Clear(&config);
+
     std::cout << Py_GetVersion() << std::endl;
     Py_Finalize();
 }
