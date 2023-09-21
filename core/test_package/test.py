250377b3ffc36e4cbce3442e0823a3e470fd8714
Dean Moldovan <dean0x7d@gmail.com>
2023-09-21 13:15:16 +0200

    Fix runtime conflict between embedded and system Python
    The issue was that our embedded Python could pick up extra Python
    packages installed in the user's home directory. Python adds user
    paths by default unless instructed otherwise. The `._pth` file puts
    it into isolated mode. See the docstrings of `_isolate()` for all
    the details.

--- core/test_package/test.py
+++ core/test_package/test.py
@@ -8,3 +8,18 @@ import uuid
 import zlib
 
 print("All optional Python features are importable")
+
+import sys
+import site
+
+if sys.version_info[:2] >= (3, 11):
+    assert sys.flags.isolated == 1
+    assert sys.flags.ignore_environment == 1
+    assert not site.ENABLE_USER_SITE
+
+print("sys.path:")
+for p in sys.path:
+    print("-", p)
+
+# The environment is isolated so only internal paths should be here
+assert all("embedded_python" in p for p in sys.path)
