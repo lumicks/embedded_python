1b3eea36c3d16bec2078dff16ee2f18f9f396aa2
Dean Moldovan <dean0x7d@gmail.com>
2024-05-03 10:08:22 +0200

    Speed up repeat includes of `embedded_python-core.cmake`
    Some projects may end up calling `find_package(embedded_python[-core])`
    multiple times in their CMake code. By extension `find_package(Python)`
    gets called many times and it's pretty slow because it needs to run
    some Python to get all the details about the interpreter. We can make
    sure that this only runs once. Conan ensures that only one version of
    `embedded_python-core` is always included in the project so the first
    info is save to cache and will not change.

--- core/embedded_python-core.cmake
+++ core/embedded_python-core.cmake
@@ -1,10 +1,12 @@
+include_guard(GLOBAL)
+
 # A hint for `find_package(Python)`
-set(Python_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/embedded_python")
+set(Python_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/embedded_python" CACHE STRING "")
 
 if(WIN32) # Extra hint to speed up the find (not needed for correctness)
-    set(Python_EXECUTABLE "${Python_ROOT_DIR}/python.exe")
+    set(Python_EXECUTABLE "${Python_ROOT_DIR}/python.exe" CACHE STRING "")
 else()
-    set(Python_EXECUTABLE "${Python_ROOT_DIR}/bin/python3")
+    set(Python_EXECUTABLE "${Python_ROOT_DIR}/bin/python3" CACHE STRING "")
 endif()
 
-find_package(Python ${self.pyversion} EXACT REQUIRED COMPONENTS Interpreter Development)
+find_package(Python ${self.pyversion} EXACT REQUIRED GLOBAL COMPONENTS Interpreter Development)
