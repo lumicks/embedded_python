ce0d683e8ce65635e96114eafc7d173b5cf1a268
Dean Moldovan <dean0x7d@gmail.com>
2024-11-11 13:21:02 +0100

    Update dependencies to the latest versions

--- test_package/numpy/test.py
+++ test_package/numpy/test.py
@@ -1,13 +1,28 @@
 """`numpy` distributes its tests: run them"""
 
+import os
 import sys
 import numpy as np
 
-# `test_mem_policy.py` and `f2py/*` tests fail with Python 3.11 due to `numpy.distutils`
-# deprecations and issues with the latest `setuptools`. Ignore it until it's resolves in `numpy`.
-sys.exit(
-    not np.test(
-        verbose=2,
-        extra_argv=["-n", "auto", "-k=not test_mem_policy and not f2py"],
-    )
-)
+# pytest's `--ignore-glob` option (see below) expands patterns as absolute paths in the
+# current folder. By default, that's our project folder, but we want to ignore some numpy
+# tests so we must target the Python prefix instead.
+os.chdir(sys.prefix)
+
+skip_tests = [
+    "test_mem_policy",  # requires `meson`
+    "test_configtool",  # `numpy-config` is not on PATH in the embedded env
+]
+
+if sys.version_info < (3, 12):
+    skip_tests += ["test_public_api"]  # fails with Python 3.11 due to `numpy.distutils`
+
+pytest_args = [
+    "-n=auto",
+    "-k=" + " and ".join([f"not {x}" for x in skip_tests]),
+    # `f2py` requires `meson` and the tests are very slow so we want to skip them. They must be
+    # excluded with `ignore-glob` to avoid even collecting them because of import errors.
+    "--ignore-glob=*f2py*",
+]
+
+sys.exit(not np.test(verbose=2, extra_argv=pytest_args))
